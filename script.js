40b03411d9767e6d206f31c9cf7bc7daf0c3c0b8
erasmojunior656 <j.erasmo@escola.pr.gov.br>
2025-07-31 08:47:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function juros() {
+    let primeiroN = parseInt(document.getElementById('primeiro').value);
+    let segundoN = parseInt(document.getElementById('segundo').value);
+    let terceiroN = parseInt(document.getElementById('terceiro').value);
+
+    let pj = segundoN / 100;
+
+    let jurossimples = primeiroN * pj * terceiroN;
+
+    document.getElementById('resultadoJ').textContent = 'Resultado: ' + jurossimples;
+}
+
+function montante() {
+
+    let primeiroN = parseInt(document.getElementById('primeiro').value);
+    let segundoN = parseInt(document.getElementById('segundo').value);
+    let terceiroN = parseInt(document.getElementById('terceiro').value);
+
+    let pj = segundoN / 100;
+
+    let jurossimples = primeiroN * pj * terceiroN;
+    
+    let m = primeiroN + jurossimples;
+
+    document.getElementById('resultadoM').textContent = 'Resultado: ' + m;
+
+}
